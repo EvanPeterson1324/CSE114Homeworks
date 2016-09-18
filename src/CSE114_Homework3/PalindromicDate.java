604089cd7674006d51ec8d1caa2c319d5d0abd8b
Evanp <Evanp@192.168.2.7>
2016-09-17 22:53:56 -0400

    Added the majority of the functionality required of part 1. Need to check to make sure all specs are met.

--- src/CSE114_Homework3/PalindromicDate.java
+++ src/CSE114_Homework3/PalindromicDate.java
@@ -6,55 +6,181 @@
 package CSE114_Homework3;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
  * @author Evanp
  */
 public class PalindromicDate {
+
     private static Scanner input; // Just a scanner
-    private static String month, day, year; // To hold the user input
-    
-    public static void main(String[] args){
+    private static String month, year, reformattedDate; // To hold the user input
+    private static int numDays;
+
+    public static void main(String[] args) {
         input = new Scanner(System.in);
-        
+        System.out.print("Enter month: ");
+        checkMonthValid();
+
+        System.out.print("Enter day: ");
+        checkValidDays();
+
+        System.out.print("Enter year: ");
+        checkValidYear();
+
+        printReformedDate();
+        printIsPalindrome();
     }
-    
-    
+
     /**
-     * This enum is to help with handling input from the user.  By making this
-     * an inner class of sorts, we can make it easier to check if input is correct.
-     * Each month has a String denoting its name and a number denoting its number.
+     * This method checks to see if the month inputed by the user is valid. If
+     * not, it keeps asking for user input.
      */
-    private enum Month{
-        JANUARY("January", 1),
-        FEBRUARY("Febuary", 2),
-        MARCH("March", 3),
-        APRIL("April", 4),
-        MAY("May", 5),
-        JUNE("June", 6),
-        JULY("July", 7),
-        AUGUST("August", 8),
-        SEPTEMBER("September", 9),
-        OCTOBER("October", 10),
-        NOVEMBER("November", 11),
-        DECEMBER("December", 12);
-        
+    private static void checkMonthValid() {
+        boolean isValid = false;
+        while (!isValid) {
+            month = input.next();
+            for (Month m : Month.values()) {
+                if (month.equalsIgnoreCase(m.getName())) {
+                    isValid = true;
+                    break;
+                }
+            }
+            if (!isValid) {
+                System.out.print("Incorrect input, please enter a valid month: ");
+            }
+        }
+    }
+
+    private static void checkValidDays() {
+        boolean isValid = false;
+        int days = Month.valueOf(month.toUpperCase()).getNumDays();
+
+        while (!isValid) {
+            try {
+                numDays = input.nextInt();
+
+                // Checks if the value of the days is less or equal to the correct number.
+                if (numDays <= days && numDays > 0) {
+                    isValid = true;
+                    break;
+                }
+
+                if (!isValid) {
+                    System.out.print("Incorrect input, please enter a valid day: ");
+                }
+            } catch(Exception e){
+                System.out.print("Incorrect input, please enter a valid day: ");
+            }
+
+        }
+
+    }
+
+    private static void checkValidYear() {
+        boolean isValid = false;
+
+        while (!isValid) {
+            year = input.next();
+            try {
+                if (year.length() > 4 || Integer.parseInt(year) < 1) {
+                    System.out.println("Incorrect input, please enter another year: ");
+                } else {
+                    isValid = true;
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Bad Input.");
+            }
+
+        }
+    }
+
+    private static void printReformedDate() {
+        // Print MM/DD/YY
+        StringBuilder sb = new StringBuilder();
+        sb.append(Month.valueOf(month.toUpperCase()).getMonthNum());
+        sb.append("/");
+
+        sb.append(numDays);
+        sb.append("/");
+
+        sb.append(year);
+
+        reformattedDate = sb.toString();
+        System.out.println("Reformatted date: " + reformattedDate);
+    }
+
+    private static void printIsPalindrome() {
+        boolean isPalendrome = isPalindrome();
+        if (isPalendrome) {
+            System.out.println("Palindrome? yes");
+        } else {
+            System.out.println("Palindrome? no");
+        }
+    }
+
+    private static boolean isPalindrome() {
+        Stack myStack = new Stack();
+
+        for (char ch : reformattedDate.toCharArray()) {
+            myStack.push(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!myStack.isEmpty()) {
+            sb.append(myStack.pop());
+        }
+
+        if (sb.toString().equals(reformattedDate)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This enum is to help with handling input from the user. By making this an
+     * inner class of sorts, we can make it easier to check if input is correct.
+     * Each month has a String denoting its name and a number denoting its
+     * number of days.
+     */
+    private enum Month {
+        JANUARY("january", 30, "01"),
+        FEBRUARY("february", 29, "02"),
+        MARCH("march", 31, "03"),
+        APRIL("april", 30, "04"),
+        MAY("may", 31, "05"),
+        JUNE("june", 30, "06"),
+        JULY("july", 31, "07"),
+        AUGUST("august", 31, "08"),
+        SEPTEMBER("september", 30, "09"),
+        OCTOBER("october", 31, "10"),
+        NOVEMBER("november", 30, "11"),
+        DECEMBER("december", 31, "12");
+
         private final String monthName;
-        private final int monthNum;
-        
-        Month(String newMonthName, int newMonthNum){
+        private final int numDays;
+        private final String monthNum;
+
+        Month(String newMonthName, int newNumDays, String newMonthNum) {
             monthName = newMonthName;
+            numDays = newNumDays;
             monthNum = newMonthNum;
         }
-        
-        private String getName(){
+
+        private String getName() {
             return monthName;
         }
-        
-        private int getMonthNum(){
+
+        private int getNumDays() {
+            return numDays;
+        }
+
+        private String getMonthNum() {
             return monthNum;
         }
-        
+
     }
 }
