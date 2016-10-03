3d5a8a4bc323c34f98be8a82ba50b43c9bdc8be3
evanpeterson <evanpeterson@10.1.172.111>
2016-10-03 16:11:58 -0400

    Inital HW #5 commit

---
+++ src/CSE114_Homework5/Homework5Methods.java
@@ -0,0 +1,35 @@
+package CSE114_Homework5;
+
+/**
+ *
+ * @author evanpeterson
+ */
+public class Homework5Methods {
+	// part 1
+	public static String eliminateDuplicates(String str) {
+		// fill this in
+		return "";
+	}
+
+	// part 2
+	public static String getDigits(String phoneNumber) {
+		// fill this in
+		return "";
+	}
+
+	// part 3
+	public static int lowestBasePalindrome(int num) {
+		// fill this in
+		return 0;
+	}
+
+	// part 4
+	public static int[] interleaveArrays(int[] array1, int[] array2, int stride, int maxElements) {
+		return null;
+	}
+	
+	// part 5
+	public static void rotateRight(int[][] matrix, int numCols) {
+		// fill this in
+	}	
+}
